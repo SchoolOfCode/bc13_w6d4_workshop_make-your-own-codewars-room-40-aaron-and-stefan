13da63ccc1252b9ad7c72836b10764f4474a7529
Aaron <22418751+ajvsol@users.noreply.github.com>
2022-11-03 14:20:14 +0000

    Completed kata

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-/* 
-👉 Write your kata here!
-*/
+// You will be given an array of numbers. Your objective is to replace multiples of 3 with "Fizz" and multiples of 5 are replaced by "Buzz". Multiples of both 3 and 5 are replaced with "FizzBuzz".
 
-//👉 Write the function your CodeWarriors will start with below here:
+// The Fizz and Buzz numbers will always be integers between 1 and 100, and the sequence will have a maximum length of 100. 
+
+// Examples
+
+// [1, 2, 3, 4, 5, 6] ==> [1, 2, "Fizz", 4, "Buzz", "Fizz"] 
+
+
+function FizzBuzzifier(array) {
+    // Enter your amazing code solution here
+}
+
+export default FizzBuzzifier;
